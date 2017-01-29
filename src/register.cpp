e2e3b567bbb6917e57440fc0b95d084b123497fe
Daniel Clarke <u8sand@gmail.com>
2017-01-29 15:47:48 -0500

    Replaced Mochi.Tray with new Qt.labs SystemTrayIcon

--- src/register.cpp
+++ src/register.cpp
@@ -4,8 +4,6 @@
 #include "player.h"
 #include "recent.h"
 #include "remote.h"
-#include "menu.h"
-#include "tray.h"
 #include "update.h"
 
 void register_qml_types() {
@@ -15,9 +13,5 @@ void register_qml_types() {
   qmlRegisterType<Player>("Mochi", 1, 0, "Player");
   qmlRegisterType<Recent>("Mochi", 1, 0, "Recent");
   qmlRegisterType<Remote>("Mochi", 1, 0, "Remote");
-  qmlRegisterType<Menu>("Mochi", 1, 0, "Menu");
-  qmlRegisterType<MenuItem>("Mochi", 1, 0, "MenuItem");
-  qmlRegisterType<MenuSeparator>("Mochi", 1, 0, "MenuSeparator");
-  qmlRegisterType<Tray>("Mochi", 1, 0, "Tray");
   qmlRegisterType<Update>("Mochi", 1, 0, "Update");
 }
